bde7326b6834a746a5c907c15c4e95b8554c397a
AHerod <metalvege@gmail.com>
2019-05-08 23:46:19 +0200

    navigation with search form

--- front-page.php
+++ front-page.php
@@ -1,14 +1,44 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <div class="container pt-5 pb-5">
-        <h1><?php the_title(); ?></h1>
+<div class="jumbotron d-flex">
+    <div class="container-fluid text-center">
+        <h1 class="display-1">Make your life
+            <span class="title-highlight">
+                            greener.
 
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-			<?php the_content(); ?>
-
-		<?php endwhile; endif; ?>
+            </span>
+        </h1>
+        <p class="lead">A global community of
+            <span class="font-weight-bolder">PLANT LOVERS.</span>
+        </p>
+        <p>Get daily news and tips from professionals and become one of them today.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </div>
 </div>
+
+<div class="container-fluid">
+    <h1>Latest blog posts</h1>
+
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <div class="shadow-sm card mb-5 ">
+            <div class="card-body">
+                <!-- Title of a post with posts -->
+                <h3><?php the_title(); ?></h3>
+
+				<?php if ( has_post_thumbnail() ): ?>
+
+                    <img src="<?php the_post_thumbnail_url( 'smallest' ) ?>" class="img-fluid"/>
+
+				<?php endif; ?>
+
+				<?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>" class="btn btn-success">Read more</a>
+            </div>
+        </div>
+	<?php endwhile; endif; ?>
+</div>
+
+
+
 <?php get_footer(); ?>
